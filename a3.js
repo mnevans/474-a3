4dc2f8f9a513ee02051e06dd52d41b8d2baf44d3
Morgan Evans <evansmorg@gmail.com>
2016-02-16 21:39:38 -0800

    remove comments

--- a3.js
+++ a3.js
@@ -8,69 +8,6 @@
 // load in CSV data
 	// then do stuff in here
 
-/*d3.select("body").append("h1").text("INFO 474 Assignment 3");
-d3.select("body").append("h3").text("Morgan Evans + Jessie Peterson");
-
-var margin = {top: 20, right:15, bottom: 60, left: 60}
-	, width = 960 - margin.left - margin.right
-	, height = 500 - margin.top - margin.bottom;
-
-var xScale = d3.scale.linear()
-			.range([0, width])		
-			//.domain()
-
-var yScale = d3.scale.linear()
-			.range([height, 0])
-			//.domain()
-
-d3.select("body").append("svg")
-	.attr("class", "chart")
-	.attr("width", width + margin.left + margin.right)
-	.attr("height", height + margin.top + margin.bottom);
-
-d3.csv("baseball.csv", type, function(error, dataset) {
-
-	console.log(dataset)
-	xScale.domain([0, d3.max(dataset, function (d){
-		return d.age
-	})])
-
-	yScale.domain([0, d3.max(dataset, function (d){
-		return d.fare
-	})])
-
-	d3.select(".chart").selectAll(".circle")
-		.data(dataset)
-		.enter().append("circle")
-		.attr("class", ".circle")
-		.attr("r", 3)
-		.attr("cx", function(d) {
-			return xScale(d.age)
-		})
-		.attr("cy", function(d) {
-			return yScale(d.fare)
-		})
-		.attr("fill", "blue")
-
-	/*var brush = d3.svg.brush()
- 		.x(x)
- 		.on("brush", brushmove)
- 		.on("brushend", brushend);*/
-
-	/*svg.append("g")
- 		.attr("class", "brush")
- 		.call(brush)
-		.selectAll('rect')
-		.attr('height', height);
-}); */
-
-/*
-function type(d) {
-	d.team = +d.team;
-	d.1985 = +d.1985;
-	return d
-} */
-
 d3.select("body").append("h1").text("INFO 474 Assignment 3");
 d3.select("body").append("h3").text("Morgan Evans + Jessie Peterson");
 
